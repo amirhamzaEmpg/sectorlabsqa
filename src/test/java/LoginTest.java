87fe0c547715805bd4dfd64906bf68b1da841af0
amir <amir.hamza@empglabs.com>
2023-07-14 12:27:20 +0500

    intial commit for automation project

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    //private String URL = "https://www.bayut.com/en";
+
+    @BeforeTest
+    public void intialization() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void loginWithValidCredentials() {
+        driver.get("https://www.bayut.com/en");
+        //1. Click login button
+        driver.findElement(By.cssSelector("[aria-label='Login']")).click();
+        //2. Enter Email
+        driver.findElement(By.cssSelector("[name='email']")).sendKeys("muhammad.haris@dubizzlelabs.com");
+        //3. Enter password
+        driver.findElement(By.id("password")).sendKeys("1234567a");
+        //4. Click login button
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        //5. verify if logged in
+        String expectedUsername = "Haris";
+        String actualUsername = driver.findElement(By.cssSelector("[aria-label='Username']")).getText();
+        Assert.assertEquals(actualUsername,expectedUsername);
+
+    }
+
+    @AfterTest
+    public void quit() {
+        driver.quit();
+    }
+}
+
